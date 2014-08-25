0566c70879ad41d3e0ccae6348b544dfa8bf9e7f
Agustin Recalde <agustinrecalde128@gmail.com>
2014-08-25 01:05:15 -0300

    Testing the file testmailer.php

--- testmailer.php
+++ testmailer.php
@@ -1,5 +1,6 @@
 <?php
 	error_reporting('E_ALL');
+	/*
 	require 'vendor/autoload.php';
 	
 	#Namespace for Amazon SES Client
@@ -10,31 +11,33 @@
     	'region'  => 'us-west-2'
 	));
 	
-	$result = $client->sendEmail(array(
-    	// Source is required
-    	'Source' => 'northwood128@gmail.com',
-    	// Destination is required
-    	 'Destination' => array(
-   	     'ToAddresses' => array('agustinrecalde128@gmail.com'),
-    	),
-    // Message is required
-    'Message' => array(
-        // Subject is required
-        'Subject' => array(
-            // Data is required
-            'Data' => 'Mail de prueba 24/08/2014',
-            'Charset' => 'utf-8',
-        ),
-        // Body is required
-        'Body' => array(
-            'Html' => array(
-                // Data is required
-                'Data' => '<b>Hola</b>',
-                'Charset' => 'utf-8',
-            ),
-        ),
-    ),
-));
+	$result = $client->sendEmail(
+		array(
+			// Source is required
+			'Source' => 'northwood128@gmail.com',
+			// Destination is required
+			'Destination' => array(
+				'ToAddresses' => array('agustinrecalde128@gmail.com'),
+			),
+			// Message is required
+			'Message' => array(
+				// Subject is required
+				'Subject' => array(
+				// Data is required
+					'Data' => 'Mail de prueba 24/08/2014',
+					'Charset' => 'UTF-8',
+				),
+				// Body is required
+					'Body' => array(
+						'Html' => array(
+						// Data is required
+   							'Data' => '<b>Hola</b>',
+   							'Charset' => 'UTF-8',
+    	    			),
+  					),
+			),
+		));
 	echo $result;
-	
+	*/
+	echo 'hola';
 ?>
