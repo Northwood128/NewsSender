27da581d0b75dc19b546b27fc3a46f91405dd2f1
Agustin Recalde <agustinrecalde128@gmail.com>
2014-08-25 00:06:22 -0300

    Testing Amazon SDK in testmailer.php

---
+++ testmailer.php
@@ -0,0 +1,40 @@
+<?php
+
+	require 'vendor/autoload.php';
+	
+	#Namespace for Amazon SES Client
+	use 'Aws\Ses\SesClient';
+	
+	$client = SesClient::factory(array(
+    	'profile' => 'default',
+    	'region'  => 'us-west-2'
+	));
+	
+	$result = $client->sendEmail(array(
+    	// Source is required
+    	'Source' => 'northwood128@gmail.com',
+    	// Destination is required
+    	 'Destination' => array(
+   	     'ToAddresses' => array('agustinrecalde128@gmail.com'),
+    	),
+    // Message is required
+    'Message' => array(
+        // Subject is required
+        'Subject' => array(
+            // Data is required
+            'Data' => 'Mail de prueba 24/08/2014',
+            'Charset' => 'utf-8',
+        ),
+        // Body is required
+        'Body' => array(
+            'Html' => array(
+                // Data is required
+                'Data' => '<b>Hola</b>',
+                'Charset' => 'utf-8',
+            ),
+        ),
+    ),
+));
+	echo $result;
+	
+?>
