3cca5da66d4807dcf8cd985ccba69d311790126b
Northwood128 <agustinrecalde128@gmail.com>
2014-09-04 18:15:13 -0700

    Mockup to try the login feature[C

--- script/login.php
+++ script/login.php
@@ -1,3 +1,14 @@
 <?php
-
+  //echo $_POST['user_mail'] . ':' . $_POST['pwd'];
+  $user_data['email'] = $_POST['user_mail'];
+  $user_data['pwd'] = $_POST['pwd'];
+ //This is just a mockup for a real DB check 
+  $valid_users = array (
+	'admin' => 'agustinrecalde128@gmail.com',
+	'user' => 'user@sender.com'
+  );
+  if (!in_array($user_data['email'],$valid_users,true))
+  {
+	header("Location: ../html/login_error");
+  }
 ?>
