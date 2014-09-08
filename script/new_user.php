73d94c94cf8a112ef435fa2fd6f64ac70351e1ac
Agustin Recalde <agustinrecalde128@gmail.com>
2014-09-08 03:10:48 -0300

    User Registration OK

--- script/new_user.php
+++ script/new_user.php
@@ -9,20 +9,34 @@
     $new_user_data['new_user_email'] = 'agustinrecalde128@gmail.com';//$_POST['new_user_email'];
     $new_user_data['new_user_name'] = 'Agustin'; //4_POST['new_user_name'];
     $new_user_data['new_user_password'] = 'admin'; //$_POST['new_user_password'];
-    $new_user_data['class'] = 'admin'; //$_POST['new_user_class'];
+    $new_user_data['new_user_class'] = 'admin'; //$_POST['new_user_class'];
     
     $correct_hash = explode(':', create_hash($new_user_data['new_user_password']));
     
     try {
         $DB_Handle = new PDO($db_settings['dns'],$db_settings['db_user'],$db_settings['db_pwd'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     } catch (PDOException $e) {
-      echo 'Error: ' , $e->getMessage(), '\n';
+        echo 'Error: ' , $e->getMessage(), '\n';
     }
     
-    $sql_users = 'INSERT INTO users (username, name, class) VALUES (:new_user_email, :new_user_name, :new_user_class)';
-    $stmt_users = $DB_handle->prepare($sql_users);
-    $stmt_users->bindParam(':new_user_email',$new_user_data['new_user_email']);
-    $stmt_users->bindParam(':new_user_name',$new_user_data['new_user_name']);
-    $stmt_users->bindParam(':new_user_class',$new_user_data['new_user_class']);
-    
+    $sql_users = "INSERT INTO users (username, name, class) VALUES (\"" . $new_user_data['new_user_email'] . "\",\"" . $new_user_data['new_user_name'] . "\",\"" . $new_user_data['new_user_class'] . "\")";
+    //echo $sql_users;
+    $affected_rows = $DB_Handle->exec($sql_users);
+    //I'll add a redirect to a nice error page eventually
+    function exception_handler($e)
+    {
+        echo 'Exception thrown: ' . $e->getMessage() . "\n";
+    }
+
+    set_exception_handler('exception_handler');
+    if ($affected_rows === 0) {
+        throw new Exception("Error Processing Request", 1);
+    } else {
+        $uid = $DB_Handle->query("SELECT uid FROM users WHERE username=\"" . $new_user_data['new_user_email'] . "\"");
+        $result = $uid->fetchObject();
+        $sql_hash = "INSERT INTO hashes (uid, hash) VALUES (\"" . $result->uid . "\",\"" . $correct_hash[HASH_PBKDF2_INDEX] . "\")";
+        $affected_rows = $DB_Handle->exec($sql_hash);
+        $sql_salt = "INSERT INTO salt (uid, salt) VALUES (\"" . $result->uid . "\",\"" . $correct_hash[HASH_SALT_INDEX] . "\")";
+        $affected_rows = $DB_Handle->exec($sql_salt);
+    }
 ?>
