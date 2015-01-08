9cd529523d4bb3090f63c270244fc1763d54599f
Agustin Recalde <agustinrecalde128@gmail.com>
2015-01-08 03:03:13 -0300

    Syncronized DB between localhost and RDS instance. Pushing tests

--- script/sendMail.php
+++ script/sendMail.php
@@ -19,51 +19,50 @@
 		
 		try {
              $DB_Handle = new PDO($dns,$settings['RDS']['username'], $settings['RDS']['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+	    	 $stmt = $DB_Handle->prepare("SELECT email FROM subscribers_test WHERE status = 'active'");
+			 $stmt->execute();
+			 $badTo = $stmt->fetchAll();
+		
+			 foreach ($badTo as $email) {
+				$to[] = $email['email'];
+			 }
+			 //App Settings: app_config.ini
+			 date_default_timezone_set('America/Argentina/Buenos_Aires');
+		
+			 $client = SesClient::factory(array(
+				 'profile' => 'default',
+    			 'region'  => 'us-west-2'
+			 ));
+		
+			 $result = $client->sendEmail(
+			 array(
+				 // Source is required
+				 'Source' => $from,
+				 // Destination is required
+				 'Destination' => array(
+					 'ToAddresses' => $to,
+				 ),
+				 // Message is required
+				 'Message' => array(
+					 // Subject is required
+					 'Subject' => array(
+					 // Data is required
+						 'Data' => $subject,
+						 'Charset' => 'UTF-8',
+					 ),
+					 // Body is required
+						 'Body' => array(
+							 'Html' => array(
+							 // Data is required
+   								 'Data' => $mailBody,
+   								 'Charset' => 'UTF-8',
+    	    				 ),
+  						 ),
+				 ),
+			 ));
         } catch (PDOException $e) {
              echo 'Error: ' . $e->getMessage() . "<br />";
 			 echo $settings['RDS']['username'] ."::". $settings['RDS']['password'];//*******************This needs to be changed
         }
-	    
-		$stmt = $DB_Handle->prepare("SELECT email FROM subscribers_test WHERE status = 'active'");
-		$stmt->execute();
-		$badTo = $stmt->fetchAll();
-		
-		foreach ($badTo as $email) {
-			$to[] = $email['email'];
-		}
-		//App Settings: app_config.ini
-		date_default_timezone_set('America/Argentina/Buenos_Aires');
-		
-		$client = SesClient::factory(array(
-			'profile' => 'default',
-    		'region'  => 'us-west-2'
-		));
-		
-		$result = $client->sendEmail(
-		array(
-			// Source is required
-			'Source' => $from,
-			// Destination is required
-			'Destination' => array(
-				'ToAddresses' => $to,
-			),
-			// Message is required
-			'Message' => array(
-				// Subject is required
-				'Subject' => array(
-				// Data is required
-					'Data' => $subject,
-					'Charset' => 'UTF-8',
-				),
-				// Body is required
-					'Body' => array(
-						'Html' => array(
-						// Data is required
-   							'Data' => $mailBody,
-   							'Charset' => 'UTF-8',
-    	    			),
-  					),
-			),
-		));
     }
 ?>
